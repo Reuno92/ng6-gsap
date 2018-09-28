a798dcf3fbeac17a5802e022a986c55b3197070f
Renaud Racinet <renaud.racinet@estiam.com>
2018-09-28 09:50:58 +0200

    initial commit
    Install package Angular6 via Angular-cli 1.6
    gsap aka greensock
    and @types/greensock tsc definition
    
    Warning: gsap script is include in angular.jon like a script in decompressed
    
    For each component where gsap is used you must be add this line:
    declare var TweenMax: any;
    
    it's require for compilation :(
    
    no ticket, no issue just trainning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Power1, Bounce} from 'gsap/all';
+
+declare var TweenMax: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'Ng6-GSAP';
+export class AppComponent implements OnInit {
+  @ViewChild('mushroom') box: ElementRef;
+
+  ngOnInit(): void {
+    this.doIt();
+  }
+
+  doIt(): void {
+    TweenMax.fromTo(this.box.nativeElement, 2, {x: 20}, {x: 440, ease: Power1.easeOut});
+    TweenMax.fromTo(this.box.nativeElement, 2, {y: 20}, {y: 440, ease: Bounce.easeOut});
+  }
 }
